c4f2130caa100b0ee16e05b2567e64a776e3da58
HrushikeshVallapu <hrushikeshvallapu>
2025-05-15 21:27:04 +0530

    hellow world

---
+++ 2-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=hrushikesh
+person2=nitish
+
+echo "hrushikesh: eh ra nitish, nuv ah roju poola kundi enduku thannav ra?"
+echo "nitish: nenu..."
+echo  "hrushikesh: ha nuvvuuu, chepra nitish"
+echo "nitish : bye ra hrushikesh"
