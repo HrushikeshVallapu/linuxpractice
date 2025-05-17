991347e8acfee7d7b7e9b4b08f0b6bb858a7706c
HrushikeshVallapu <hrushikeshvallapu>
2025-05-17 18:38:43 +0530

    day 2

---
+++ 4-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ echo "variables passed : $@"
+ echo " total no of variables : $#"
+ echo "name of the script:$0"
+ echo "current working dirctry :$PWD"
+ echo "current user : $USER"
+ echo "PID of current script :$$"
+ sleep 5 &
+ echo "PID of last command ran : $!"
