92c89da5fa8d80ba9daf183c27642b939276c93a
HrushikeshVallapu <hrushikeshvallapu>
2025-05-19 22:23:09 +0530

    day 3 of shell scripting

--- 11-logs.sh
+++ 11-logs.sh
@@ -5,7 +5,7 @@ r="\e[31m"
 g="\e[32m"
 y="\e[33m"
 n="\e[0m"
-logs_folder="etc/log/shellscript-logs"
+logs_folder="var/log/shellscript-logs"
 script_name=$(echo $0 | cut -d "." -f1)
 log_file="$logs_folder/$script_name.log"
 
