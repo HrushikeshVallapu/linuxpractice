75f2ccf2fd0c81f828f59cbf8416be57ac5e54b5
HrushikeshVallapu <hrushikeshvallapu>
2025-05-19 22:27:20 +0530

    day 3 of shell scripting

--- 11-logs.sh
+++ 11-logs.sh
@@ -10,22 +10,22 @@ script_name=$(echo $0 | cut -d "." -f1)
 log_file="$logs_folder/$script_name.log"
 
 mkdir -p $logs_folder
-echo "script started executing at $(date)" &>>$log_file
+echo "script started executing at $(date)" | tee -a $log_file
 
 if [ $userid -ne 0 ]
 then 
-    echo -e "$r you are not root user $n" &>>$log_file
+    echo -e "$r you are not root user $n" | tee -a $log_file
     exit 1
 else
-    echo "preparing to start the installation" &>>$log_file
+    echo "preparing to start the installation" | tee -a $log_file
 fi
 
 validate(){
     if [ $1 -eq 0 ]
     then 
-        echo -e "$g installation of $2 success $n" &>>$log_file
+        echo -e "$g installation of $2 success $n" | tee -a $log_file
     else   
-        echo -e "$r installation of $2 failed $n" &>>$log_file
+        echo -e "$r installation of $2 failed $n" | tee -a $log_file
         exit 1
     fi
 }
@@ -36,7 +36,7 @@ then
     dnf install mysql -y &>>$log_file
     validate $? "mysql"
 else
-    echo -e "package $y mysql already exist $n" &>>$log_file
+    echo -e "package $y mysql already exist $n" | tee -a $log_file
 fi
 
 dnf list installed python3 &>>$log_file
@@ -45,7 +45,7 @@ then
     dnf install python3 -y &>>$log_file
     validate $? "python3"
 else
-    echo -e "package $y python3 already exist $n" &>>$log_file
+    echo -e "package $y python3 already exist $n" | tee -a $log_file
 fi
 
 dnf list installed nginx &>>$log_file
@@ -54,5 +54,5 @@ then
     dnf install nginx -y &>>$log_file
     validate $? "nginx"
 else
-    echo -e "package $y nginx already exist $n" &>>$log_file
+    echo -e "package $y nginx already exist $n" | tee -a $log_file
 fi
