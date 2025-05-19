1fedc73148547c95d5afeffdf1503708912cd603
HrushikeshVallapu <hrushikeshvallapu>
2025-05-19 22:20:45 +0530

    day 3 of shell scripting

---
+++ 11-logs.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+userid=$(id -u)
+r="\e[31m"
+g="\e[32m"
+y="\e[33m"
+n="\e[0m"
+logs_folder="etc/log/shellscript-logs"
+script_name=$(echo $0 | cut -d "." f1)
+log_file="$logs_folder/$script_name.log"
+
+mkdir -p $logs_folder
+echo "script started executing at $(date)" &>>$log_file
+
+if [ $userid -ne 0 ]
+then 
+    echo -e "$r you are not root user $n" &>>$log_file
+    exit 1
+else
+    echo "preparing to start the installation" &>>$log_file
+fi
+
+validate(){
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$g installation of $2 success $n" &>>$log_file
+    else   
+        echo -e "$r installation of $2 failed $n" &>>$log_file
+        exit 1
+    fi
+}
+
+dnf list installed mysql &>>$log_file
+if [ $? -ne 0 ]
+then 
+    dnf install mysql -y &>>$log_file
+    validate $? "mysql"
+else
+    echo -e "package $y mysql already exist $n" &>>$log_file
+fi
+
+dnf list installed python3 &>>$log_file
+if [ $? -ne 0 ]
+then 
+    dnf install python3 -y &>>$log_file
+    validate $? "python3"
+else
+    echo -e "package $y python3 already exist $n" &>>$log_file
+fi
+
+dnf list installed nginx &>>$log_file
+if [ $? -ne 0 ]
+then 
+    dnf install nginx -y &>>$log_file
+    validate $? "nginx"
+else
+    echo -e "package $y nginx already exist $n" &>>$log_file
+fi
